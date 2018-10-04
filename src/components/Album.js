1b074c9f75def704e990541d977a96a227b1891a
Matt <mknickelbine@gmail.com>
2018-10-03 19:13:57 -0500

    Assignment 8: Player Buttons -- Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -55,8 +55,19 @@ class Album extends Component {
   }
 
   handlePrevClick(song) {
+    if (!this.state.isPlaying) {return};
     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
     const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
+  handleNextClick(song) {
+    if (!this.state.isPlaying) {return};
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(this.state.album.songs.length, currentIndex + 1);
+    if (currentIndex >= this.state.album.songs.length - 1) {return};
     const newSong = this.state.album.songs[newIndex];
     this.setSong(newSong);
     this.play();
@@ -119,6 +130,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={ () => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={ () => this.handlePrevClick() }
+          handleNextClick={ () => this.handleNextClick() }
         />
       </section>
     )
